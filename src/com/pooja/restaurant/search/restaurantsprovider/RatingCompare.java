a15bb9dccf6460b53dac0858189fda928cc35327
Pooja Khandelwal <er.pooja112@gmail.com>
2017-10-15 01:23:15 +0530

    Rating comparator bug fix

--- src/com/pooja/restaurant/search/restaurantsprovider/RatingCompare.java
+++ src/com/pooja/restaurant/search/restaurantsprovider/RatingCompare.java
@@ -7,14 +7,12 @@ public class RatingCompare implements Comparator<Restaurant> {
 	@Override
 	public int compare(Restaurant r1, Restaurant r2) {
 		// TODO Auto-generated method stub
-		int result;
-		if (r1.getRatingValue() < r2.getRatingValue()) {
+		int result = Double.compare(r1.getRatingValue(), r2.getRatingValue());
+		if (result == 1) {
 			result = -1;
-		}	
-		if (r1.getRatingValue() > r2.getRatingValue()) {
+		} else if (result == -1) {
 			result = 1;
-		}
-		else {
+		} else if (result == 0) {
 			// result is zero means ratings are equal, so sorting based on name
 			result = r1.getName().compareTo(r2.getName());
 		}
